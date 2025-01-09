dc4c87cd1e7141892513e1f4d858027a099dd741
danib13 <danibm@me.com>
2025-01-08 17:25:18 -0800

    Add files via upload

---
+++ HealthBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    /* assign health bar image in Inspector */
+    [SerializeField] private Image healthFill;
+
+    public void UpdateHealthBar(float current, float max)
+    {
+        /* update according to the total health left */
+        healthFill.fillAmount = current/max;
+    }
+}
